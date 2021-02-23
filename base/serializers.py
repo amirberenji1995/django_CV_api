9ef169e2e55811982d2be97cfe8612e1877ddf12
Amir Hossein Berenji <a_h_berenji@yahoo.com>
2021-02-23 23:53:50 +0330

    Face Detection Finished

--- base/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from models import BaseImage
-
-
-class BaseSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = BaseImage
-        fields = ('original', 'description', 'uploaded_at', 'processed')
-
