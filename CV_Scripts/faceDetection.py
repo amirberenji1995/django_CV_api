8cc8f8f70c0e2faa9b8e2c058013310a3837d308
Amir Hossein Berenji <a_h_berenji@yahoo.com>
2021-03-14 22:44:56 +0330

    Face Detection migrated to the OpenCV

--- CV_Scripts/faceDetection.py
+++ CV_Scripts/faceDetection.py
@@ -1,18 +1,15 @@
-import face_recognition
-from PIL import Image
 import cv2
 
 
 def face_detector(add, file):
+    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
     file_name = file.split('/')[-1]
-    image = face_recognition.load_image_file(add + file)
-    face_locations = face_recognition.face_locations(image)
-    face_rois = []
-    for face_location in face_locations:
-        top, right, bottom, left = face_location
-        face_image = image[top:bottom, left:right]
-        face_rois.append(Image.fromarray(face_image).convert('LA').resize((48,48)))
-        image = cv2.rectangle(image, (left, top), (right, bottom), (255,0 , 0), 15)
-        image = Image.fromarray(image)
-        image.save(add + '/media/' + 'processed_' + file_name)
+    img = cv2.imread(add + file)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+
+    cv2.imwrite(add + '/media/' + 'processed_' + file_name, img)
     return 'processed_' + file_name
